33dd5ada671bb4251faad62be094215c44b1492e
cabbott008 <curtisabbott@me.com>
2024-07-17 13:06:08 -0500

    oil tweaks

--- gerg/lua/gerg/oil.lua
+++ gerg/lua/gerg/oil.lua
@@ -1,23 +1,21 @@
-local function is_always_hidden(name)
-  if name == ".git" then return true end
-
-  return false
-end
+-- local function is_always_hidden(name)
+--   if name == ".git" then return true end
+--
+--   return false
+-- end
 
 require("oil").setup({
   columns = { "icon", "permissions", "size", "mtime", },
   view_options = {
+    -- is_always_hidden = is_always_hidden,
     show_hidden = true,
-    is_always_hidden = is_always_hidden,
+    natural_order = true,
+    case_insensitive = true,
   },
+  constrain_cursor = "editable",
+  delete_to_trash = true,
+  watch_for_changes = true,
 })
 
 vim.keymap.set("n", "<leader>tt", "<CMD>Oil<CR>", { desc = "Open containing directory" })
 
--- local wk = require("which-key")
--- wk.add({
---   o = {
---     name = "oil file explorer",
---     ["."] = { "<cmd>Oil<cr>", "Open containing directory", },
---   },
--- }, { prefix = "<leader>", })
