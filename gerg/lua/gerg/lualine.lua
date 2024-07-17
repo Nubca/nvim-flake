8ab60f337199f9b65350c2ac236942c96f090239
cabbott008 <curtisabbott@me.com>
2024-07-17 04:51:39 -0500

    fonts and tweaks

--- gerg/lua/gerg/lualine.lua
+++ gerg/lua/gerg/lualine.lua
@@ -2,7 +2,7 @@
 require("lualine").setup({
   options = {
     icons_enabled = true,
-    theme = "auto",
+    theme = "onedark",
     component_separators = { "", "" },
     section_separators = { "", "" },
     disabled_filetypes = { "packer", "NvimTree", "alpha" },
