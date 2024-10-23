046cc841bff79fe67865248fd5f4171eba7a0e26
cabbott008 <curtisabbott@me.com>
2024-10-23 18:26:50 -0500

    glitch

--- gerg/lua/gerg/dashboard.lua
+++ gerg/lua/gerg/dashboard.lua
@@ -25,7 +25,6 @@
           key = 'd',
         },
       },
-      packages = { enable = false },
       project = { enable = true, limit = 5, icon = ' ', label = 'Recent Projects', action = 'Telescope find_files cwd='},
       mru = { limit = 10, icon = ' ', label = 'Recent Files', cwd_only = false },
       footer = {"\nWe do this NOT because it is easy,\nbut because we thought it would be easy."
