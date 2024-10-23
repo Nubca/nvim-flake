57603c9f87684e2d1b41e9c0dee0b98c03ff0f28
cabbott008 <curtisabbott@me.com>
2024-10-23 18:48:42 -0500

    another

--- gerg/lua/gerg/dashboard.lua
+++ gerg/lua/gerg/dashboard.lua
@@ -25,7 +25,6 @@ require('dashboard').setup {
         key = 'd',
       },
     },
-    packages = { enable = false },
     project = { enable = true, limit = 5, icon = ' ', label = 'Recent Projects', action = 'Telescope find_files cwd='},
     mru = { limit = 10, icon = ' ', label = 'Recent Files', cwd_only = false },
     footer = {
