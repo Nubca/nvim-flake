45c06fc883967ff6243875a50c882034ff902cb6
cabbott008 <curtisabbott@me.com>
2024-10-23 18:36:41 -0500

    fix

--- gerg/lua/gerg/dashboard.lua
+++ gerg/lua/gerg/dashboard.lua
@@ -1,33 +1,33 @@
-  require('dashboard').setup {
-    theme = 'hyper',
-    config = {
-      week_header = {
-       enable = true,
+require('dashboard').setup {
+  theme = 'hyper',
+  config = {
+    week_header = {
+     enable = true,
+    },
+    shortcut = {
+      {
+        icon = ' Find',
+        icon_hl = '@variable',
+        group = 'Label',
+        action = 'Telescope find_files',
+        key = 'f',
       },
-      shortcut = {
-        {
-          icon = ' Find',
-          icon_hl = '@variable',
-          group = 'Label',
-          action = 'Telescope find_files',
-          key = 'f',
-        },
-        {
-          desc = ' Apps',
-          group = 'DiagnosticHint',
-          action = 'Telescope app',
-          key = 'a',
-        },
-        {
-          desc = ' dotfiles',
-          group = 'Number',
-          action = 'Telescope dotfiles',
-          key = 'd',
-        },
+      {
+        desc = ' Apps',
+        group = 'DiagnosticHint',
+        action = 'Telescope app',
+        key = 'a',
       },
-      project = { enable = true, limit = 5, icon = ' ', label = 'Recent Projects', action = 'Telescope find_files cwd='},
-      mru = { limit = 10, icon = ' ', label = 'Recent Files', cwd_only = false },
-      footer = {"\nWe do this NOT because it is easy,\nbut because we thought it would be easy."
+      {
+        desc = ' dotfiles',
+        group = 'Number',
+        action = 'Telescope dotfiles',
+        key = 'd',
       },
     },
-  }
+    packages = { enable = false },
+    project = { enable = true, limit = 5, icon = ' ', label = 'Recent Projects', action = 'Telescope find_files cwd='},
+    mru = { limit = 10, icon = ' ', label = 'Recent Files', cwd_only = false },
+    footer = { "We do this NOT because it is easy, but because we thought it would be easy."},
+  },
+}
