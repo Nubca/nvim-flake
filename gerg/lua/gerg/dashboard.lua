407320dfcbf043df2caa8bfe68f2499560196c65
cabbott008 <curtisabbott@me.com>
2024-10-23 17:33:31 -0500

    dashboard work

--- gerg/lua/gerg/dashboard.lua
+++ gerg/lua/gerg/dashboard.lua
@@ -1,3 +1,33 @@
-require('dashboard').setup {
-  theme = 'doom'
-    }
+  require('dashboard').setup {
+    theme = 'hyper',
+    config = {
+      week_header = {
+       enable = true,
+      },
+      shortcut = {
+        {
+          icon = ' Find',
+          icon_hl = '@variable',
+          desc = 'Files',
+          group = 'Label',
+          action = 'Telescope find_files',
+          key = 'f',
+        },
+        {
+          desc = ' Apps',
+          group = 'DiagnosticHint',
+          action = 'Telescope app',
+          key = 'a',
+        },
+        {
+          desc = ' .files',
+          group = 'Number',
+          action = 'Telescope dotfiles',
+          key = 'd',
+        },
+      },
+      project = { enable = true, limit = 5, icon = '', label = 'Recent Projects', action = 'Telescope find_files cwd='},
+      mru = { limit = 10, icon = '', label = 'Recent Files', cwd_only = false },
+      footer = {"We do this NOT because it is easy, but because we thought it would be easy."},
+    },
+  }
