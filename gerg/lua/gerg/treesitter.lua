6634a8de244eb500f404b44f1d3016a9035e7dca
cabbott008 <curtisabbott@me.com>
2024-11-03 20:27:06 -0600

    turn off ibl

--- gerg/lua/gerg/treesitter.lua
+++ gerg/lua/gerg/treesitter.lua
@@ -5,37 +5,37 @@ require("nvim-treesitter.configs").setup({
   ignore_install = {},
   ensure_installed = {},
   auto_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+  -- highlight = {
+  --   enable = true,
+  --   additional_vim_regex_highlighting = false,
+  -- },
 })
 
--- rainbows
-local highlight = {
-  "RainbowDelimiterRed",
-  "RainbowDelimiterYellow",
-  "RainbowDelimiterBlue",
-  "RainbowDelimiterOrange",
-  "RainbowDelimiterGreen",
-  "RainbowDelimiterViolet",
-  "RainbowDelimiterCyan",
-}
-vim.g.rainbow_delimiters = {
-  highlight = highlight,
-}
-local hooks = require("ibl.hooks")
-hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-
-require("ibl").setup({
-  scope = {
-    enabled = true,
-    show_start = true,
-    show_end = true,
-    injected_languages = true,
-    highlight = highlight,
-  },
-  indent = {
-    char = "┋",
-  },
-})
+-- -- rainbows
+-- local highlight = {
+--   "RainbowDelimiterRed",
+--   "RainbowDelimiterYellow",
+--   "RainbowDelimiterBlue",
+--   "RainbowDelimiterOrange",
+--   "RainbowDelimiterGreen",
+--   "RainbowDelimiterViolet",
+--   "RainbowDelimiterCyan",
+-- }
+-- vim.g.rainbow_delimiters = {
+--   highlight = highlight,
+-- }
+-- local hooks = require("ibl.hooks")
+-- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+--
+-- require("ibl").setup({
+--   scope = {
+--     enabled = true,
+--     show_start = true,
+--     show_end = true,
+--     injected_languages = true,
+--     highlight = highlight,
+--   },
+--   indent = {
+--     char = "┋",
+--   },
+-- })
