dc98596fe3c2c605f177bbc67266238b5e049195
Curtis Abbott <inspiredplans@gmail.com>
2025-11-30 16:06:39 -0600

    update

--- lua/lazy/oil.lua
+++ lua/lazy/oil.lua
@@ -1,6 +1,17 @@
 return {
   "oil.nvim",
   lazy = false,
+  opts = {
+    default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,  -- Always show hidden/dotfiles
+      is_always_hidden = function(name, bufnr) return false end,
+      is_always_hidden = function(name, _)
+          return name == "node_modules" or name == ".git"
+        end,
+    },
+  },
   after = function()
     require("oil").setup({
       keymaps = {
