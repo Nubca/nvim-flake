4920de9a96bdedd7198fe85e249c7d8a3d8dc071
Curtis Abbott <inspiredplans@gmail.com>
2026-05-27 19:56:46 -0500

    treesitter fix

--- lua/gerg/treesitter.lua
+++ lua/gerg/treesitter.lua
@@ -1,8 +1,23 @@
 vim.g._ts_force_sync_parsing = true
+
+local disabled_filetypes = { "markdown", "quarto", "rmd" }
+
+local function disable_treesitter(lang, buf)
+  if vim.tbl_contains({ "markdown", "markdown_inline" }, lang) then
+    return true
+  end
+
+  return vim.tbl_contains(disabled_filetypes, vim.bo[buf].filetype)
+end
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = vim.treesitter.language._complete(),
   group = vim.api.nvim_create_augroup("LoadTreesitter", {}),
-  callback = function()
+  callback = function(args)
+    if vim.tbl_contains(disabled_filetypes, vim.bo[args.buf].filetype) then
+      return
+    end
+
     vim.treesitter.start()
   end,
 })
@@ -14,6 +29,7 @@ require("nvim-treesitter").setup({
   auto_install = false,
   highlight = {
     enable = true,
+    disable = disable_treesitter,
     additional_vim_regex_highlighting = false,
   },
 })
